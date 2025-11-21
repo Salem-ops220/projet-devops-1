9142e83c6a2e84f73f7c34ae94bfc58a99fa7618
salem <salemajimi22@gmail.com>
2025-11-21 18:42:31 +0100

    init

--- src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
+++ src/test/java/tn/esprit/studentmanagement/StudentManagementApplicationTests.java
@@ -3,11 +3,14 @@ package tn.esprit.studentmanagement;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(properties = {
+        "spring.jpa.hibernate.ddl-auto=none",
+        "spring.datasource.url=jdbc:h2:mem:testdb"
+})
 class StudentManagementApplicationTests {
 
     @Test
     void contextLoads() {
     }
-
 }
+
